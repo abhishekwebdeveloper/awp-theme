6eefe676babb0045a791cb8c0bcab934f7f194aa
Abhishek Pandey <abhishek.pandey@madebyaura.com>
2026-09-12 00:28:59 +0530

    Add middle section to product single view

--- plugins/Woocommerce/views/sections/single/index.php
+++ plugins/Woocommerce/views/sections/single/index.php
@@ -11,3 +11,5 @@ use abhishekWebDeveloper\AwpTheme\Base;
 defined( 'ABSPATH' ) || die();
 
 Base::view( 'plugins/woocommerce/views/sections/single/hero' );
+Base::view( 'plugins/woocommerce/views/sections/single/middle' );
+Base::view( 'core/views/sections/home/faq' );
---
+++ plugins/Woocommerce/views/sections/single/middle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Middle section for the product single.
+ *
+ * @package abhishekWebDeveloper\AwpTheme
+ * @author  abhishekWebDeveloper
+ */
+
+defined( 'ABSPATH' ) || die();
+?>
+
+<div class="awp-container awp-product__container flex flex-col gap-8 lg:gap-12 xl:flex-row xl:gap-15 2xl:gap-20">
+	<div class="awp-product__main flex-1">Main</div>
+
+	<aside class="awp-product__sidebar shrink-0 w-90 hidden lg:block">
+		<div class="awp-product__sidebar-content sticky top-[calc(30px+var(--awp-admin-bar-height,0px))]">
+			Sidebar
+		</div>
+	</aside>
+</div>
